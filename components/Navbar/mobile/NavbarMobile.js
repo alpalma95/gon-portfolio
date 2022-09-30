85775487d71deac3a377f6e03e8508737b8e0761
Al <alpalma95@gmail.com>
2022-09-30 14:01:31 +0200

    changed map

--- components/Navbar/mobile/NavbarMobile.js
+++ components/Navbar/mobile/NavbarMobile.js
@@ -6,8 +6,6 @@ import { useContext } from "react";
 import { Context } from "../../../store/store";
 
 import styles from "./NavbarMobile.module.scss";
-import bars from "../../../public/assets/bars.png";
-import cross from "../../../public/assets/cross.svg";
 
 const NavbarMobile = () => {
   const { store, actions } = useContext(Context);
@@ -21,7 +19,7 @@ const NavbarMobile = () => {
       <div className={styles.navbarMobileContainer}>
         <div className={styles.toggle} onClick={actions.setNavbarOpen}>
           {store.navbarOpen ? (
-            <Image src={cross} alt="realistic heart within a circle" />
+            <i className="fa-solid fa-xmark"></i>
           ) : (
             <i className="fa-solid fa-bars"></i>
           )}
